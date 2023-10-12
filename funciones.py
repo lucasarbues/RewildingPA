94b3669b964aa5476e1df50269a2d5382a9c8be9
abrilnoguera <anoguera@itba.edu.ar>
2023-10-12 10:55:50 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -2,7 +2,10 @@ import re
 import boto3
 import cv2
 import numpy as np
+import io
 import pytesseract
+from PIL import Image
+from PIL.ExifTags import TAGS   
 
 
 s3 = boto3.client('s3')
@@ -39,8 +42,23 @@ def definirRuta(fila):
                     ruta += fila['Nombre'][0] + '/' + fila['Nombre']
             elif fila['Camara'] == 'S2': # IMG_0001.JPG
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
-                segmentos = str(int(fila['Nombre'].split('.')[1][0])+3)
-                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/4/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                if fila['Nombre'][0] == '5':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/5/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                else:    
+                    # segmentos = str(int(fila['Nombre'].split('.')[1][0])+3)
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/4/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+            
+            elif fila['Camara'] == 'S3': # IMG_0001.JPG
+                resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                if fila['Nombre'][0] == '2':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/2/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                elif fila['Nombre'][0] == '3':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/3/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                else:    
+                    # segmentos = str(int(fila['Nombre'].split('.')[1][0])+3)
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+            
+            
             elif fila['Camara'] == 'S4': # IMG_0001.JPG
                 ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara'])
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
@@ -56,10 +74,32 @@ def definirRuta(fila):
                     ruta += '/IMG_' + resultado.group(1).zfill(4) + '.JPG'
             elif fila['Camara'] == 'S5': 
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
-                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/1 - S5 - (' + resultado.group(1) + ').JPG'
-            elif (fila['Camara'] == 'S7') or (fila['Camara'] == 'S8'): 
+                if fila['Nombre'][0] == '2':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/1 - S5 - (' + resultado.group(1) + ').JPG'
+                else:
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/2 - S5 - (' + resultado.group(1) + ').JPG'
+            
+            elif fila['Camara'] == 'S6': 
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
-                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                if fila['Nombre'][0] == '2':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/2 - S6 - (' + resultado.group(1) + ').JPG'
+                else:
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/1-S6- (' + resultado.group(1) + ').JPG'
+            
+            elif (fila['Camara'] == 'S7'): 
+                resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                if fila['Nombre'][0] == '2':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/2/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                elif fila['Nombre'][0] == '0':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                else:
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+            elif (fila['Camara'] == 'S8'):
+                if fila['Fecha'] in ('2022-03-11','2022-03-15'): # ESTAS IMAGENES CON ESTAS FECHAS NO EXISTEN ESAS FECHAS EN S8
+                    ruta = ''
+                else:
+                    resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1/IMG_' + resultado.group(1).zfill(4) + '.JPG'
             elif (fila['Camara'] == 'S9'):  
                 if fila['Nombre'] == 'IMG_0788.JPG':
                     ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/100_BTCF/IMG_0788.JPG'
@@ -69,6 +109,26 @@ def definirRuta(fila):
             elif (fila['Camara'] == 'S10') or (fila['Camara'] == 'S11'):  
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
                 ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+            
+            elif (fila['Camara'] == 'S13'):
+                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/'
+                if fila['Nombre'][0] == '1':
+                    ruta += '1/' + str(fila['Nombre'][1:])
+                elif fila['Nombre'][0] == 'S': #2/2-S13- (426).JPG
+                    resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                    ruta += '2/2-S13- (' + resultado.group(1) + ').JPG'
+                else:
+                    ruta += fila['Nombre']
+            
+            elif (fila['Camara'] == 'S14'):
+                resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/'
+                if fila['Nombre'][0] == 'S':
+                    ruta += '1/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                elif fila['Nombre'][0]=='2':
+                    ruta += '2/2-S14- (' + resultado.group(1) + ').JPG'
+                else:
+                    ruta += 'IMG_' + resultado.group(1).zfill(4) + '.JPG'
 
             elif (fila['Camara'] == 'S15'):
                 ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/'
@@ -78,10 +138,41 @@ def definirRuta(fila):
                     resultado = re.search(r'\((.*?)\)', str(fila['Nombre']))
                     ruta += 'IMG_' + str(resultado.group(1).zfill(4)) + '.JPG'
                 else:
-                    ruta += '1/' + str(fila['Nombre'])
-            elif (fila['Camara'] == 'S18') or (fila['Camara'] == 'S19'): 
+                    ruta += '1/' + str(fila['Nombre'])      
+            elif (fila['Camara'] == 'S16'):
+                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/'
+                if fila['Nombre'][0] == '1':
+                    ruta += '1/' + str(fila['Nombre'][1:])
+                elif fila['Nombre'][0] == '2':
+                    ruta += '2/' + str(fila['Nombre'][1:])    
+                else:
+                    ruta += fila['Nombre']
+
+            elif (fila['Camara'] == 'S17'):
+                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/'
+                if fila['Nombre'][0] == '1':
+                    ruta += '1/' + str(fila['Nombre'][1:])
+                elif fila['Nombre'][0] == '5':
+                    ruta += '5/' + str(fila['Nombre'][1:])    
+                elif fila['Nombre'][0] == '4':
+                    ruta += '4/' + str(fila['Nombre'][1:])    
+                else:
+                    ruta += fila['Nombre']
+
+            elif (fila['Camara'] == 'S18'): 
                 resultado = re.search(r'\((.*?)\)', fila['Nombre'])
-                ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1 - ' + fila['Camara'] + ' - (' + resultado.group(1) + ').JPG'
+                if fila['Nombre'][0] == '0':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+                else:
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1 - ' + fila['Camara'] + ' - (' + resultado.group(1) + ').JPG'
+            
+            elif fila['Camara'] == 'S19': 
+                resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                if fila['Nombre'][0] == '2':
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/2 - S19 - (' + resultado.group(1) + ').JPG'
+                else:
+                    ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/1 - ' + fila['Camara'] + ' - (' + resultado.group(1) + ').JPG'
+            
             else:
                 ruta += 'sauce/CT S ' + str(fila['Año']) + '/' + str(fila['Camara']) + '/' + fila['Nombre']
         else:
@@ -93,8 +184,11 @@ def definirRuta(fila):
         if fila['Año'] == 2021:
             ruta += fila['Camara'] + '/' + fila['Nombre']
         elif fila['Año'] == 2022:
-            resultado = re.search(r'\((.*?)\)', fila['Nombre'])
-            ruta += 'Ib-22 (' + str(fila['Camara'][2]) + ')/IMG_' + resultado.group(1).zfill(4) + '.JPG'
+            if (fila['Camara'] == 'IB1') and (fila['Nombre'][4] == 'b'):
+                return ''
+            else:
+                resultado = re.search(r'\((.*?)\)', fila['Nombre'])
+                ruta += 'Ib-22 (' + str(fila['Camara'][2]) + ')/IMG_' + resultado.group(1).zfill(4) + '.JPG'
         else:
             raise ValueError("No existe esa ruta.")
     else:
@@ -105,6 +199,7 @@ def definirRuta(fila):
     if ruta in objetos_s3:
         return ruta
     return ''
+
 
 # Configurar la ruta de ejecutable de Tesseract (reemplaza con la ubicación en tu sistema)
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract' ## HAY QUE INSTALAR TESSARACT (en la terminal: brew install tesseract)
@@ -203,3 +298,41 @@ def extraerHora(imagen_path):
         return fecha_extraida
     
     return ''
+
+# Función para obtener los datos de una imagen en S3
+def extraerFechaHora(image_key):
+    if image_key != '':
+        try:
+            # Obtiene el objeto de la imagen desde S3
+            response = s3.get_object(Bucket='s3-pfa', Key=image_key)
+            # Obtiene el cuerpo del objeto (que contiene los datos de la imagen)
+            image_data = response['Body'].read()
+
+            # A continuación, puedes procesar los datos de la imagen como desees.
+            # Por ejemplo, puedes usar una biblioteca como Pillow (PIL) para trabajar con la imagen.
+
+            # Aquí puedes agregar tu código para procesar la imagen y extraer los datos de metadatos.
+            # Por ejemplo, si quieres extraer datos EXIF:
+        
+            image = Image.open(io.BytesIO(image_data))
+            exif_data = image._getexif()
+            
+            # Crea un diccionario para almacenar los datos de metadata
+            metadata = {}
+
+            # Itera a través de los datos EXIF y los almacena en el diccionario
+            for tag, value in exif_data.items():
+                tag_name = TAGS.get(tag, tag)
+                metadata[tag_name] = value
+
+
+            fechahora = metadata.get('DateTime')
+            fecha, hora = fechahora.split(' ')
+
+            # Devuelve los datos de metadatos en el formato que necesites (por ejemplo, como un diccionario)
+            return fecha, hora  # Cambia esto según tus necesidades
+
+        except Exception as e:
+            print(f'Error al procesar la imagen {image_key}: {str(e)}')
+            return '',''
+    return '',''
