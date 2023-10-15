edb82aba383dd6ae8e0c05884e8537b71d24b9e8
abrilnoguera <anoguera@itba.edu.ar>
2023-10-15 11:16:27 -0300

    Delete ai4eutils

--- funciones.py
+++ funciones.py
@@ -3,6 +3,7 @@ import boto3
 import cv2
 import numpy as np
 import io
+import tensorflow as tf
 from PIL import Image
 from PIL.ExifTags import TAGS   
 import pickle
@@ -230,3 +231,25 @@ def extraerFechaHora(image_key):
             print(f'Error al procesar la imagen {image_key}: {str(e)}')
             return '',''
     return '',''
+
+def load_and_convert_image(img_path):
+    # Leer y decodificar la imagen
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_image(img, channels=3, expand_animations=False)  # asumimos imágenes en color (3 canales)
+    
+    # Cambiar el tamaño de la imagen a 150x150
+    img_resized = tf.image.resize(img, [150, 150])
+    
+    return tf.convert_to_tensor(img_resized)
+
+def get_date_time_from_image(path):
+    """Extrae la fecha y hora de la metadata de una imagen."""
+    try:
+        with Image.open(path) as img:
+            exif_data = img._getexif()
+            if exif_data and 36867 in exif_data:  # 36867 es el tag para DateTimeOriginal
+                date_time = exif_data[36867]
+                date, time = date_time.split(" ")
+                return date, time
+    except Exception as e:
+        print(f"Error processing image {path}: {e}")
