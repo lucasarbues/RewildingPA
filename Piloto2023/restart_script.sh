08afbe71da452be76dad292d8e8fc27d99dcec24
abrilnoguera <anoguera@itba.edu.ar>
2023-12-01 22:57:22 -0300

    Create restart_script.sh

---
+++ Piloto2023/restart_script.sh
@@ -0,0 +1,6 @@
+while true
+do
+   python Piloto2023/ui_ejecucion_modelos.py
+   echo "Reiniciando script después de una pausa..."
+   sleep 300  # Espera 5 minutos (300 segundos) antes de reiniciar
+done
